933a202a0bc66e63170ebca20a190a34561fb506
Ayushman Tomar <141900459+AyushmanTomar@users.noreply.github.com>
2024-08-11 00:24:40 +0530

    update

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/encryptpdf')
+def encryptpdf():
+    return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
